caad780818ef2a31851fad9b73887ddce1d55b3a
Nataliiyaa <agupovanatasha05@gmail.com>
2024-10-20 10:25:15 +0300

    drawEllipse

--- Task2/RasterizationFXApp/src/main/java/com/cgvsu/rasterization/Rasterization.java
+++ Task2/RasterizationFXApp/src/main/java/com/cgvsu/rasterization/Rasterization.java
@@ -18,4 +18,54 @@ public class Rasterization {
             for (int col = x; col < x + width; ++col)
                 pixelWriter.setColor(col, row, color);
     }
+
+    public static void drawEllipse(
+            final GraphicsContext graphicsContext,
+            final int a, final int b,
+            final int centerX, final int centerY,
+            Color color)
+    {
+        final PixelWriter pixelWriter = graphicsContext.getPixelWriter();
+
+        int x=0;
+        int y=b;
+        int i=a*a+b*b-(2*b*a*a);
+        int limit =0;
+        int del,deld;
+
+        while(y>=limit){
+            pixelWriter.setColor(x+centerX, y+centerY, color);
+            pixelWriter.setColor(-x+centerX, -y+centerY, color);
+            pixelWriter.setColor(x+centerX, -y+centerY, color);
+            pixelWriter.setColor(-x+centerX, y+centerY, color);
+
+            if(i<0){
+                del=2*i+2*y-1;
+                if(del<=0){
+                    x=x+1;
+                    i=i+2*x*b*b+b*b;
+                }
+                else{
+                    x=x+1;
+                    y=y-1;
+                    i=i+(2*x*b*b)-(2*y*a*a)+a*a+b*b;
+                }
+
+            }
+            else{
+                deld=2*(i-x)-1;
+                if(deld<=0){
+                    x=x+1;
+                    y=y-1;
+                    i=i+2*(x-y+1);
+                }
+                else{
+                    y=y-1;
+                    i=i-2*y*a*a+a*a;
+                }
+
+            }
+        }
+
+    }
 }
